8534ca8f90e60fc72c766aeb7015959d4fe75413
Tiago Ribeiro <tiagofr98@gmail.com>
2020-12-17 16:36:10 -0300

    Componentes

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// Decorator - selector: id, templateUrl: interface
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
